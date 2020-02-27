dd413d89dbc11a3d5c487699bc7fe0a30fb624f5
Samuel Schlesinger <github@samuelms.io>
2020-02-26 22:32:13 -0700

    Minor style tweaks.

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,6 +13,7 @@ import './theme.scss';
 import './layout.scss';
 
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Layout = () => {
     const [fadeIn] = useState(true);
@@ -23,26 +24,38 @@ const Layout = () => {
             <div className="portfolio-container no-user-select">
                 <Carousel className="full-size" axis="vertical" showThumbs={false} showArrows={false} showIndicators={false} useKeyboardArrows={true} showStatus={false}>
                     <div className="full-size">
-                        <CSSTransition classNames="portfolio" in={fadeIn} appear={true} timeout={2500}>
+                        <CSSTransition classNames="portfolio" in={fadeIn} appear={true} timeout={1500}>
                             <div className="full-size position-relative">
                                 <div className="full-size intro-background position-absolute position-left position-top" />
                                 <div className="row no-margins center-xs middle-xs full-size intro-container position-relative">
                                     <div className="position-relative intro-content">
                                         <div className="position-absolute position-left position-top">
                                             <CSSTransition classNames="header-main" in={shiftHeaders} timeout={700}>
-                                                <Typist startDelay={2500} avgTypingDelay={30} stdTypingDelay={5} cursor={{ show: false }} onTypingDone={() => setShiftHeaders(true)}>
-                                                    <h1 className="no-margins text-size-2 text-color-accent text-align-left">SOFTWARE ARCHITECT AND PRODUCT OWNER</h1>
+                                                <Typist startDelay={1200} avgTypingDelay={30} stdTypingDelay={5} cursor={{ show: false }} onTypingDone={() => setShiftHeaders(true)}>
+                                                    <h1 className="no-margins text-size-2 text-color-accent text-align-left portfolio-text-shadow">SOFTWARE ARCHITECT AND PRODUCT OWNER</h1>
                                                 </Typist>
                                             </CSSTransition>
                                         </div>
                                         <h1 className="no-margins text-size-2 text-color-accent no-height hidden">SOFTWARE ARCHITECT AND PRODUCT OWNER</h1>
                                         <CSSTransition classNames="header-secondary" in={shiftHeaders} timeout={800}>
-                                            <h2 className="no-margins text-color-light top-spacer-small header-secondary-enter">FOCUSED ON HIGH-POWER STARTUPS AND MASSIVE ENTERPRISE</h2>
+                                            <h2 className="no-margins text-color-light top-spacer-small portfolio-text-shadow header-secondary-enter">FOCUSED ON HIGH-POWER STARTUPS AND MASSIVE ENTERPRISE</h2>
                                         </CSSTransition>
                                         <CSSTransition classNames="header-content" in={shiftHeaders} timeout={1800}>
-                                            <p className="text-color-light text-weight-bold top-spacer-large header-content-enter">I love figuring out how to build software products. Put me in a room with stakeholders, your best people, and SMEs – and I'll roadmap out a strategy to get you from where you are now to where you need to be.</p>
+                                            <p className="text-color-light text-weight-bold top-spacer-large portfolio-text-shadow header-content-enter">I love figuring out how to build software products. Put me in a room with stakeholders, SMEs, and your best people – and I'll roadmap out a strategy to get you from where you are now to where you need to be.</p>
                                         </CSSTransition>
                                     </div>
+                                </div>
+                                <div className="row no-margins center-xs bottom-xs full-size position-absolute position-bottom position-left">
+                                    <CSSTransition classNames="next-slide-indicator" in={shiftHeaders} timeout={2750}>
+                                        <div className="text-color-accent next-slide-indicator-enter">
+                                            <div className="row no-margins center-xs">
+                                                <small className="text-color-accent text-weight-bold next-slide-indicator-label portfolio-text-shadow">WHO AM I?</small>
+                                            </div>
+                                            <div className="row no-margins center-xs">
+                                                <FontAwesomeIcon className="next-slide-indicator-icon" icon="caret-down" size="2x" fixedWidth />
+                                            </div>
+                                        </div>
+                                    </CSSTransition>
                                 </div>
                             </div>
                         </CSSTransition>
@@ -51,9 +64,6 @@ const Layout = () => {
                     <div className="full-size"></div>
                     <div className="full-size"></div>
                 </Carousel>
-                <footer className="row no-margins full-size bottom-xs center-xs full-size position-absolute position-left position-top no-pointer-events text-color-light">
-                    <small className="bottom-spacer-small">© {new Date().getFullYear()} – Samuel Schlesinger</small>
-                </footer>
             </div>
         </>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,16 @@ import 'normalize.css/normalize.css';
 import 'flexboxgrid2/flexboxgrid2.min.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
 import '../components/fonts.scss';
 
 import SEO from '../components/seo';
 import Layout from '../components/layout';
 
+
+library.add(fas);
 
 const IndexPage = () => (
     <>
